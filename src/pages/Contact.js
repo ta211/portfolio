b01831957a03b916ade41c377a601407b9e10495
ta211 <55717622+ta211@users.noreply.github.com>
2022-04-26 12:39:17 -0400

    contact left page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,17 @@
+import './Contact.scss';
+
+const contactLeft = 
+<div className="contact-left-container container">
+    <div className="contact-email">
+        Email: atang1@andrew.cmu.edu
+    </div>
+    <div className="contact-github">
+        <a href="https://github.com/ta211">Github</a>
+    </div>
+</div>
+
 const contact = {
-    left: <div></div>,
+    left: contactLeft,
     right: <div></div>
 }
 export default contact;
