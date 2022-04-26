8e2ee31e3d41da877388305f3d0d07adcbfafd56
ta211 <55717622+ta211@users.noreply.github.com>
2022-04-26 12:21:43 -0400

    About Me left - half completed

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,5 +1,18 @@
+import './AboutMe.scss'
+
+const aboutmeLeft = 
+<div className="aboutme-left-container">
+    <div className="aboutme-intro">
+        Hello! This is An Tang.
+        <br/>
+        I am a AI and HCI student at Carnegie Mellon University in my third undergraduate year.
+        <br/>
+        And I am bad at writing intro, so you can ignore this block, but please check out the photo on the right though, it is a sliding game.
+    </div>
+</div>;
+
 const aboutme = {
-    left: <div></div>,
+    left: aboutmeLeft,
     right: <div></div>
 }
 export default aboutme;
