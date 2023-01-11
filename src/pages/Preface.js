09f944f1f2c70d7c3bbf74e2f51ef4ddad7ea4f7
An Tang <antang06211@gmail.com>
2023-01-11 00:36:00 -0500

    Add Preface page education info & resume

--- src/pages/Preface.js
+++ src/pages/Preface.js
@@ -1,4 +1,5 @@
 import './Preface.scss'
+import resume from '../media/Resume-A4.pdf';
 
 const prefaceLeft = 
 <div className="preface-left-container container">
@@ -9,7 +10,19 @@ const prefaceRight =
 <div className="preface-right-container container">
     <div className="preface-arrow" />
     <div className="preface-arrow-lower"/>
+    <div className="photo">
+
+    </div>
     <div className="education">
+        Carnegie Mellon Univserity <br/>
+        Aug 2019 - May 2023 | Pittsburgh <br/>
+        B.S. in 
+        <ul>
+            <li>Aritificial Intelligence</li>
+            <li>Human-Computer Interaction</li>
+        </ul>
+        GPA: 3.80 <br />
+        <a href={resume} className="link">Link to resume</a>
     </div>
 </div>
 
