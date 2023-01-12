4037610146bd4badba1c1d7ecaeb1ae7879ddded
An Tang <antang06211@gmail.com>
2023-01-12 08:41:55 -0500

    Added Preface selfie + details -> deployed

--- src/pages/Preface.js
+++ src/pages/Preface.js
@@ -1,5 +1,6 @@
 import './Preface.scss'
 import resume from '../media/Resume-A4.pdf';
+import selfie from '../media/selfie_fencing.jpg';
 
 const prefaceLeft = 
 <div className="preface-left-container container">
@@ -10,19 +11,21 @@ const prefaceRight =
 <div className="preface-right-container container">
     <div className="preface-arrow" />
     <div className="preface-arrow-lower"/>
-    <div className="photo">
-
+    <div className="photo-container">
+        <img className="selfie" src={selfie} alt={"A photo of me at my school fencing club"} />
     </div>
-    <div className="education">
-        Carnegie Mellon Univserity <br/>
-        Aug 2019 - May 2023 | Pittsburgh <br/>
-        B.S. in 
-        <ul>
-            <li>Aritificial Intelligence</li>
-            <li>Human-Computer Interaction</li>
-        </ul>
-        GPA: 3.80 <br />
-        <a href={resume} className="link">Link to resume</a>
+    <div className="education-container">
+        <div className="education">
+            Carnegie Mellon Univserity <br/>
+            Aug 2019 - May 2023 | Pittsburgh <br/>
+            B.S. in 
+            <ul>
+                <li>Aritificial Intelligence</li>
+                <li>Human-Computer Interaction</li>
+            </ul>
+            GPA: 3.80 <br />
+            <a href={resume} className="link" target="_blank">Link to resume</a>
+        </div>
     </div>
 </div>
 
